d1ce5662d40d1ea41d2e17ae85938329bd777cce
alexanderalban <albanalexander@gmail.com>
2021-01-19 17:03:21 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      arrayOfBeer: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.punkapi.com/v2/beers')
+      .then( res => {
+        const arrayOfBeer = res.data
+        this.setState({ arrayOfBeer })
+      })
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*map of this.state.arrayOfBeer */}
       </header>
     </div>
   );
+}
 }
 
 export default App;
